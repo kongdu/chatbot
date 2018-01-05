36ee25d3b7a019360f65566eaa6150f66b56fe67
kongdu <yeonyya@gmail.com>
2018-01-05 02:29:21 +0000

    메뉴기능추가

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -8,6 +8,12 @@ class KakaoController < ApplicationController
 
   def message
     user_message = params[:content]
+
+    if user_message == '메뉴'
+      menus = ["20층", "시골집", "순남시래기", "양자강", "두부마을"]
+      user_message = menus.sample
+    end
+
     return_message = {
       :message => {
         :text => user_message
