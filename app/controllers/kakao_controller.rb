0565188ebf07f56f726b287f065af30b66fa1428
kongdu <yeonyya@gmail.com>
2018-01-05 02:40:48 +0000

    로또기능 추가

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -12,6 +12,11 @@ class KakaoController < ApplicationController
     if user_message == '메뉴'
       menus = ["20층", "시골집", "순남시래기", "양자강", "두부마을"]
       user_message = menus.sample
+    elsif user_message == '로또'
+      lotto = (1..45).to_a.sample(6).to_s
+      user_message = lotto
+    else
+      user_message = "사용 가능한 명령어 : '메뉴', '로또'를 입력해보세요"
     end
 
     return_message = {
